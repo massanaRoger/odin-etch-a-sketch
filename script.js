64b2476261a707797aae580e2ee21c225b8c625e
Roger Massana <rogermassana2002@gmail.com>
2022-07-26 15:13:27 +0200

    Add 16*16 grid square

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < COLUMNS * ROWS; i++) {
     let cell = document.createElement('div');
+    cell.classList.add('cell');
     container.appendChild(cell);
 }
