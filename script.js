da089bbbc6fe4b04e5904ec91ec0da77f7c18bbe
Roger Massana <rogermassana2002@gmail.com>
2022-07-26 15:45:45 +0200

    Add button to select size of grid

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const COLUMNS = 16;
-const ROWS = 16;
 const container = document.querySelector('.container');
 
-for (let i = 0; i < COLUMNS * ROWS; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    container.appendChild(cell);
-}
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach(addTransition);
+document.querySelector('.chose-size').addEventListener('click', promptMessage);
 
 function addTransition(cell) {
     cell.addEventListener('mouseover', function () {
         this.classList.add('cell-hover');
     });
+}
+
+function promptMessage() {
+    document.querySelectorAll('.cell').forEach(cell => {
+        cell.remove();
+    });
+    let n = prompt('Enter the number of rows and columns');
+    if (n > 0 && n <= 100) {
+        createGrid(n);
+    } else {
+        promptMessage();
+    }
+}
+
+function createGrid(n) {
+    for (let i = 0; i < n * n; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+    container.setAttribute('style', `grid-template-columns: repeat(${n}, 1fr);`);
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(addTransition);
 }
