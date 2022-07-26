8e0c58ccda0473035706a13d11df3cc3b6a694a1
Roger Massana <rogermassana2002@gmail.com>
2022-07-26 15:05:01 +0200

    Create a loop to insert elements to container

---
+++ script.js
@@ -0,0 +1,8 @@
+const COLUMNS = 16;
+const ROWS = 16;
+const container = document.querySelector('.container');
+
+for (let i = 0; i < COLUMNS * ROWS; i++) {
+    let cell = document.createElement('div');
+    container.appendChild(cell);
+}
